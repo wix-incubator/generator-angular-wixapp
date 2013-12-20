5f308b08e9c6aa87009a1cb9caf99673e352417c
Tautvilas <mecinskas@gmail.com>
2013-12-20 15:53:18 +0200

    watch for scss updatees

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -57,7 +57,7 @@ module.exports = function (grunt) {
         },
         files: [
           '<%%= yeoman.app %>/{,*/}*.html',
-          '.tmp/styles/{,*/}*.css',
+          '.tmp/styles/{,*/}*.{css,scss}',
           '<%%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
         ]
       }
