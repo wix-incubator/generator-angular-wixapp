ee2063225bbc96fc41ccbef1b48e8547a18027ea
tautvilas <tautvilas@wix.com>
2014-10-16 17:58:43 +0300

    fix(translations)

--- app/templates/app/scripts/settings.js
+++ app/templates/app/scripts/settings.js
@@ -19,21 +19,6 @@ angular.module('appSettings', [
       prefix: 'translations/settings_',
       suffix: '.json'
     });
-  })
-  .run(function ($wix, $translate) {
-    // Version 1.1.1 of angular-translate has a bug with async
-    // loading, last language loaded overrides your selection here. To
-    // have predictable behavior we use simple approach with promise
-    // value produced by $translate.uses(...) instead of
-    // .preferredLanguage() or .fallbackLanguage().
-    var locale = $wix.Utils.getLocale();
-    if (!locale) {
-      locale = 'en';
-    }
-    $translate.uses(locale).then(function success() {
-        // translations loaded, nothing to do.
-    }, function error() {
-      // fallback language.
-      $translate.uses('en');
-    });
+
+    $translateProvider.preferredLanguage('en');
   });
